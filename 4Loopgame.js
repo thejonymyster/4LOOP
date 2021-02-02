a68c9756f9c55b9fb1120b4e18213906c5cdafe9
TheJonyMyster <thejonymyster@gmail.com>
2021-02-02 14:37:41 -0500

    fix syntax error

--- 4Loopgame.js
+++ 4Loopgame.js
@@ -16,40 +16,24 @@ const winCombo = [
 ];
 
 class Box {
-<<<<<<< HEAD
-    constructor(win, array) {
-        this.win = 0
-        this.arr = ["", "", "", "", "", "", "", "", ""]
-    }
-
-    checkWin() {
-        for (i = 0; i < 8; i++) {
-            if ((this.arr[winCombo[i][0]] == this.arr[winCombo[i][1]]) && (this.arr[winCombo[i][1]] == this.arr[winCombo[i][2]])) {
-=======
     constructor(win, arr) {
       this.win = 0
       this.arr = ["", "", "", "", "", "", "", "", ""]
     }
+  
 
-  checkWin() {
+   checkWin() {
     for (let i=0; i<8 ;i++){
         if ((this.arr[winCombo[i][0]] == this.arr[winCombo[i][1]]) && (this.arr[winCombo[i][1]] == this.arr[winCombo[i][2]])){
             if ((this.arr[winCombo[i][0]] !== "") && (this.arr[winCombo[i][1]] !== "") && (this.arr[winCombo[i][2]] !== "")) {
->>>>>>> 6a31d8131995704a7c2f8ca6678839e7c504bf30
                 return true
             }
         }
         return false
     }
 }
-
-<<<<<<< HEAD
-function changeTurn() {
-    turn = 1 - turn
 }
 
-
-=======
 let Box1 = new Box(0,[])
 
 document.querySelector(".game-box").onclick = function(e){
@@ -71,4 +55,3 @@ document.querySelector(".small-box").onclick = function(e){
       }
 console.log(this.classList)
 }
->>>>>>> 6a31d8131995704a7c2f8ca6678839e7c504bf30
