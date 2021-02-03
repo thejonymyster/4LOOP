8d5fda099b339162816479bb94427cbfe3068b8f
Rastko <rastkodjurica@yahoo.com>
2021-02-03 17:04:56 -0500

    class box cells DOM

--- script.js
+++ script.js
@@ -1,25 +1,51 @@
-// class Cell {
-//     constructor() {
-//         this.x = null
-//         this.o = null
 
-//     }
-//     draw() {
-//         let td = document.createElement("td")
-//         console.log(td)
+let turn = 0;
 
-//         td.onclick = function () {
-//             this.innerHTML = 'hi'
-//         }
+class Box {
+    constructor() {
+        this.x = null
+        this.o = null
+        this.pointerEvents = false;
+    }
+    draw(x) {
+        let div = document.createElement('div')
+        div.classList.add('small-box')
+        document.body.querySelector('.game-box').append(div)
 
-//         document.querySelector("body").append(td)
-//     }
-// }
+        let table = document.createElement('table')
+        div.append(table)
+        for (let i = 0; i < 3; i++) {
+            let tr = document.createElement('tr')
+            table.append(tr)
+            for (let j = 0; j < 3; j++) {
+                new Cell(tr).draw()
+            }
+
+        }
+
+    }
+}
+
+
+class Cell {
+    constructor(row) {
+        this.x = null
+        this.o = null
+        this.row = row
+
+    }
+    draw() {
+        let td = document.createElement("td")
+        td.onclick = function () {
+            turn++
+            this.innerHTML = turn % 2 ? 'X' : 'O'
+        }
+        this.row.append(td)
+    }
+}
 
 
 
-
-// for (let i = 0; i < 50; i++) {
-//     new Cell().draw()
-
-// }
+for (i = 1; i < 10; i++) {
+    new Box().draw(i)
+}
