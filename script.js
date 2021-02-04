061f4e6d7c76e31a3c4b44d9824c97df2a0e4375
thejonymyster <thejonymyster@gmail.com>
2021-02-04 12:49:18 -0500

    please work LOL

--- script.js
+++ script.js
@@ -18,37 +18,48 @@ class Box {
     constructor(rowNum) {
         this.x = null
         this.o = null
-        this.rowNum = rowNum
-        this.pointerEvents = false;
+
     }
-    draw(x) {
-        let div = document.createElement('div')
-        div.classList.add('small-box')
-        document.body.querySelector('.game-box').append(div)
+    draw() {
+        let td = document.createElement("td")
+        console.log(td)
 
-        let table = document.createElement('table')
-        div.append(table)
-        for (let i = 1; i < 4; i++) {
-            let tr = document.createElement('tr')
-            table.append(tr)
-            for (let j = 1; j < 4; j++) {
-                new Cell(tr, i, j, this.rowNum).draw()
-            }
-
+        td.onclick = function () {
+            this.innerHTML = 'hi'
         }
 
-    }
-}
+        document.querySelector("body").append(td)
+
+        let turn = 0;
+
+        class Box {
+            constructor(rowNum) {
+                this.x = null
+                this.o = null
+                this.rowNum = rowNum
+                this.pointerEvents = false;
+            }
+            draw(x) {
+                let div = document.createElement('div')
+                div.classList.add('small-box')
+                document.body.querySelector('.game-box').append(div)
+
+                let table = document.createElement('table')
+                div.append(table)
+                for (let i = 1; i < 4; i++) {
+                    let tr = document.createElement('tr')
+                    table.append(tr)
+                    for (let j = 1; j < 4; j++) {
+                        new Cell(tr, i, j, this.rowNum).draw()
+                    }
+
+                }
+
+            }
+        }
 
 
-class Cell {
-    constructor(row, block, microBlock, rowNum) {
-        this.x = null
-        this.o = null
-        this.rowNum = rowNum
-        this.row = row
-        this.block = block;
-        this.microBlock = microBlock
+
 
     }
     draw() {
